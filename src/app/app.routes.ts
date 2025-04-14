9672b5d993244e6e2ce96c5c1bff873d8ce9ab9c
tetsudevelopment <bresmacawork@gmail.com>
2025-04-14 10:56:23 -0500

    Implementación del reto: Desarrollo de menú interactivo para 'A la carta' con integración de API, cálculo de acumulativos y validación de platos veganos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
-import { loginRedirectGuard } from './login-redirect.guard';
-import { authGuard } from './auth.guard';
+
+import { authGuard } from './core/auth.guard';
+import { loginRedirectGuard } from './core/login-redirect.guard';
 
 export const routes: Routes = [
   {
--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -1,6 +1,7 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from './services/auth.service';
+import { AuthService } from '../services/auth.service';
+
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
@@ -9,7 +10,7 @@ export const authGuard: CanActivateFn = (route, state) => {
   if (authService.isLogged()) {
     return true;
   }
-  
+
   router.navigate(['/login']);
   return false;
 };
